d50caaa0e89ada114a41650f9fde0be71fb0fa78
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-07-03 17:23:02 -0700

    Add HomeController.cs

---
+++ FriendLetter/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendLetter.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/hello")]
+        public string Hello()
+        {
+            return "Hello friend!";
+        }
+
+        [Route("/goodbye")]
+        public string Goodbye()
+        {
+            return "Goodbye friend.";
+        }
+
+        [Route("/")]
+        public string Letter()
+        {
+            return "Our virtual postcard will go here soon!";
+        }
+
+
+    }
+}
